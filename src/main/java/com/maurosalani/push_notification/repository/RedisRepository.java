365de1531c12201cb7c01f10f3b7cfafbdd42ef7
Matteo Mauro <mauro.matteo96@gmail.com>
2020-02-22 11:20:25 +0100

    formatted code

--- src/main/java/com/maurosalani/push_notification/repository/RedisRepository.java
+++ src/main/java/com/maurosalani/push_notification/repository/RedisRepository.java
@@ -1,4 +1,4 @@
- package com.maurosalani.push_notification.repository;
+package com.maurosalani.push_notification.repository;
 
 import java.util.Collection;
 
@@ -12,82 +12,82 @@ import com.maurosalani.push_notification.dto.Subscription;
 
 public class RedisRepository {
 
-	private final String PORT = "6379";
-	private final String URL = "redis://127.0.0.1:";
-	private final String TOPIC_USERNAME = "topic_username";
-	private final String USERNAME_SUBSCRIPTION = "username_subscription";
-	final RMultimap<String, String> topic_username; // package scope for IT test
-	final RMap<String, Subscription> username_subscription; // package scope for IT test
+    private final String PORT = "6379";
+    private final String URL = "redis://127.0.0.1:";
+    private final String TOPIC_USERNAME = "topic_username";
+    private final String USERNAME_SUBSCRIPTION = "username_subscription";
+    final RMultimap<String, String> topic_username; // package scope for IT test
+    final RMap<String, Subscription> username_subscription; // package scope for IT test
 
-	public RedisRepository() {
-		Config config = new Config();
-		config.useSingleServer().setAddress(URL + PORT);
-		RedissonClient redisson = Redisson.create(config);
-		topic_username = redisson.getSetMultimap(TOPIC_USERNAME);
-		username_subscription = redisson.getMap(USERNAME_SUBSCRIPTION);
-	}
+    public RedisRepository() {
+        Config config = new Config();
+        config.useSingleServer().setAddress(URL + PORT);
+        RedissonClient redisson = Redisson.create(config);
+        topic_username = redisson.getSetMultimap(TOPIC_USERNAME);
+        username_subscription = redisson.getMap(USERNAME_SUBSCRIPTION);
+    }
 
-	public void subscribeUserToTopic(String topic, String username) {
-		topic_username.put(topic, username);
-	}
+    public void subscribeUserToTopic(String topic, String username) {
+        topic_username.put(topic, username);
+    }
 
-	public void unsubscribeUserFromTopic(String topic, String username) {
-		topic_username.remove(topic, username);
-	}
+    public void unsubscribeUserFromTopic(String topic, String username) {
+        topic_username.remove(topic, username);
+    }
 
-	public Collection<String> getAllUsernameFromTopic(String topic) {
-		return topic_username.getAll(topic);
-	}
+    public Collection<String> getAllUsernameFromTopic(String topic) {
+        return topic_username.getAll(topic);
+    }
 
-	public void unsubscribeUsernameFromAllTopics(String username) {
-		topic_username.keySet().stream().forEach(topic -> unsubscribeUserFromTopic(topic, username));
-	}
+    public void unsubscribeUsernameFromAllTopics(String username) {
+        topic_username.keySet().stream().forEach(topic -> unsubscribeUserFromTopic(topic, username));
+    }
 
-	public void registerUser(Subscription subscription) {
-		username_subscription.put(subscription.getUsername(), subscription);
-	}
+    public void registerUser(Subscription subscription) {
+        username_subscription.put(subscription.getUsername(), subscription);
+    }
 
-	public void unregisterUserByEndpoint(String endpoint) {
-                Subscription subscription = username_subscription.values().stream()
-                                    .filter(sub -> sub.getEndpoint().equals(endpoint)).findFirst().get();
-              	if (subscription != null) {
-			String username = subscription.getUsername();
-			removeUsernameFromTopics(username);
-			username_subscription.remove(username);
-		}
-	}
+    public void unregisterUserByEndpoint(String endpoint) {
+        Subscription subscription = username_subscription.values().stream()
+                .filter(sub -> sub.getEndpoint().equals(endpoint)).findFirst().get();
+        if (subscription != null) {
+            String username = subscription.getUsername();
+            removeUsernameFromTopics(username);
+            username_subscription.remove(username);
+        }
+    }
 
-	public void unregisterUserByUsername(String username) {
-		if (username_subscription.containsKey(username)) {
-			removeUsernameFromTopics(username);
-			username_subscription.remove(username);
-		}
-	}
+    public void unregisterUserByUsername(String username) {
+        if (username_subscription.containsKey(username)) {
+            removeUsernameFromTopics(username);
+            username_subscription.remove(username);
+        }
+    }
 
-	private void removeUsernameFromTopics(String username) {
-		for (String topic : topic_username.keySet()) {
-			if (topic_username.get(topic).contains(username)) {
-				topic_username.remove(topic, username);
-			}
-		}
-	}
+    private void removeUsernameFromTopics(String username) {
+        for (String topic : topic_username.keySet()) {
+            if (topic_username.get(topic).contains(username)) {
+                topic_username.remove(topic, username);
+            }
+        }
+    }
 
-	public boolean isSubscribed(String endpoint) {
-            
-                boolean isSubscribed = username_subscription.values().stream()
-                    .anyMatch(sub -> sub.getEndpoint().equals(endpoint));
-		return isSubscribed;
-	}
+    public boolean isSubscribed(String endpoint) {
 
-	public String getPORT() {
-		return PORT;
-	}
+        boolean isSubscribed = username_subscription.values().stream()
+                .anyMatch(sub -> sub.getEndpoint().equals(endpoint));
+        return isSubscribed;
+    }
 
-	public String getURL() {
-		return URL;
-	}
+    public String getPORT() {
+        return PORT;
+    }
 
-	public Subscription getSubscriptionFromUsername(String username) {
-		return username_subscription.get(username);
-	}
+    public String getURL() {
+        return URL;
+    }
+
+    public Subscription getSubscriptionFromUsername(String username) {
+        return username_subscription.get(username);
+    }
 }
